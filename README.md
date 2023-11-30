bd856ea5a06fd32dccdf40ee019722487d106b0d
Roche Olivier <rolivier@mmltd.co.za>
2023-11-30 12:24:30 +0200

    Updated page
    Updated page

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# cloud-out-loud-security
-JWT and passport active directory
+# NodeJS - JWT and AD passport active directory strategy.
+First draft
+
+## ABOUT
+- Wrapper method of Matt Robenolt [Matt Robenolt JWT package](https://www.npmjs.com/package/jwt) 
+- Wrapper method of Jared Hanson [Jared Hanson passport package](https://www.npmjs.com/package/passport) 
+- Wrapper method of Branden Horiuchi [Branden Horiuchi passport-activedirectory package](https://www.npmjs.com/package/passport-activedirectory) 
+- FIRST DRAFT
+
+## Documentation & FAQ
+The documentation & FAQ can be found at [Documentation and frequently asked questions (FAQ)](https://cloudoutloud.net/#/cloud-out-loud-security)  
+
+## Source code
+The source can be found at [Github](https://github.com/Roche-Olivier/cloud-out-loud-security)  
+
+## Issues
+Issues can be logged at [Github issues](https://github.com/Roche-Olivier/cloud-out-loud-security/issues)  
+
+
+## License
+Copyright (c) 2010-2030 Roche Olivier (roche.olivier@outlook.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,135 @@
+const path = require('path')
+const jwt = require('jsonwebtoken');
+const passport = require('passport')
+const ActiveDirectoryStrategy = require('passport-activedirectory')
+
+exports._jwt = {
+    init_express_session: function(app) {
+        var express_session = require('express-session')
+        var is_secure = false
+        if (process.env.APP_ENV === "DEVELOPMENT" || process.env.APP_ENV === "TEST" || process.env.APP_ENV === "PREPROD" || process.env.APP_ENV === "PRODUCTION") {
+            is_secure = true
+        }
+        app.use(express_session({
+            secret: process.env.SESSION_COOKIE_SECRET,
+            resave: false,
+            saveUninitialized: true,
+            // Mitigate identification of tools used
+            name: process.env.SESSION_COOKIE_NAME,
+            cookie: {
+                path: "/", // Cookies can only be gotten from this site
+                httpOnly: true, // Cookie is not accessable to script on the client side, mititgates XXS attacks
+                secure: is_secure, // Only allow cookies to be presented over HTTPS
+                maxAge: process.env.SESSION_COOKIE_TIMEOUT_MS * 1
+            }
+        }));
+    },
+    authorize: function(req, res, next) {
+
+        var token = '';
+        //check headers
+        var authHeader = req.headers.authorization;
+        if (authHeader) {
+            token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.JWT_COOKIE_SECRET, (err, user) => {
+                if (err) {
+                    res.sendFile(path.join(__dirname, '../', '/content/pages/login.html'));
+                }
+                req.user = user;
+                next();
+            });
+        } else {
+            // check for a cookie
+            var cookies = req.headers.cookie
+            if (cookies) {
+                var cookie_list = cookies.split(';')
+                var found = false
+                var found_val = ''
+                if (cookie_list.length > 0) {
+                    cookie_list.forEach(element => {
+                        var cookie = element.trim()
+                        var cookie_parts = cookie.split('=')
+                        if (cookie_parts[0] === process.env.JWT_COOKIE_NAME) {
+                            found = true
+                            found_val = cookie_parts[1]
+                            token = cookie_parts[1]
+                        }
+                    });
+
+                    if (found) {
+                        jwt.verify(token, process.env.JWT_COOKIE_SECRET, (err, user) => {
+                            if (err) {
+                                res.clearCookie(process.env.JWT_COOKIE_NAME);
+                                res.sendFile(path.join(__dirname, '../', '/content/pages/login.html'));
+                            } else {
+                                req.user = user;
+                                next();
+                            }
+                        });
+                    } else {
+                        res.sendFile(path.join(__dirname, '../', '/content/pages/login.html'));
+                    }
+                }
+            } else {
+                res.sendFile(path.join(__dirname, '../', '/content/pages/login.html'));
+            }
+        }
+
+    },
+    sign:function(payload,secret,options){
+        jwt.sign(payload,secret,options)
+    }
+}
+
+exports._ad = {
+    passport: function (app) {
+
+        //PASSPORT
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        passport.serializeUser(function (user, done) {
+            done(null, user);
+        });
+        passport.deserializeUser(function (user, done) {
+            done(null, user);
+        });
+        passport.use(new ActiveDirectoryStrategy({
+            integrated: false,
+            ldap: {
+                url: process.env.AD_URL,
+                baseDN: process.env.AD_BASE_DN,
+                username: process.env.AD_UN,
+                password: process.env.AD_PW
+            }
+        }, function (profile, ad, done) {
+            var profile_dn = profile._json.dn
+            profile_dn = profile_dn.replace("=", "%3D") // escape this else it gives and error on the isusermemberof
+            ad.isUserMemberOf(profile_dn, 'AccessGroup', function (err, isMember) {
+                if (err) {
+                    return done(err)
+                } else {
+                    // console.log(profile)
+                    if (profile) {
+                        if (profile._json) {
+                            if (profile._json.displayName) {
+                                console.log("User logged on : " + profile._json.displayName)
+                            }
+                        }
+                    }
+                    return done(null, profile)
+                }
+            })
+        }, function (err) {
+            //res.status(401).send('Not Authenticated')
+            var jsondata = {
+                "success": "false",
+                "code": "401",
+                "error": ""
+            }
+            res.json(jsondata);
+        }))
+
+
+    },
+}
